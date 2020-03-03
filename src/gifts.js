70edfcd8096ad4a9480079a7c65a5bbc32daef51
Dmitry <riumkin@gmail.com>
2020-03-03 19:22:00 +0300

    add immer

--- src/gifts.js
+++ src/gifts.js
@@ -1,5 +1,22 @@
-export const addGift = (state, description, image) => state
+import produce from 'immer'
 
-export const toggleReservation = (state, giftId) => {
-  return state
-}
+export const addGift = (state, id, description, image) =>
+  produce(state, draft => {
+    draft.gifts.push({
+      id,
+      description,
+      image,
+      reservedBy: undefined,
+    })
+  })
+
+export const toggleReservation = (state, giftId) =>
+  produce(state, draft => {
+    const gift = draft.gifts.find(gift => gift.id === giftId)
+    gift.reservedBy =
+      gift.reservedBy === undefined
+        ? state.currentUser.id
+        : gift.reservedBy === state.currentUser.id
+        ? undefined
+        : gift.reservedBy
+  })
